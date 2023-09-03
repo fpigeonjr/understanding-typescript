a12d3dc8964c46f7068af0136aaba28a761551cf
fpigeonjr <frank.pigeonjr@gmail.com>
2023-09-03 00:51:16 -0500

    tuples

--- app.ts
+++ app.ts
@@ -1,9 +1,23 @@
 const person: {
   name: string
   age: number
+  hobbies: string[]
+  role: [number, string] //tuple
 } = {
   name: "Frank",
-  age: 43
+  age: 43,
+  hobbies: ["bowling", "swimming", "running", "hiking"],
+  role: [2, "author"]
 }
 
-console.log(person.age)
+person.role.push("admin") //not caught by TS
+person.role[1] = 10 //not caught by TS
+
+let favActivities: string[]
+favActivities = ["bowling", "swimming", "running", "hiking"]
+
+console.log({ person })
+
+for (const hobby of person.hobbies) {
+  console.log(`${hobby.toUpperCase()} is a hobby of ${person.name}`)
+}
